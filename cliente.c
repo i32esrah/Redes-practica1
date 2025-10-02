8deaf44b8a29f9fc969dea0211ed0c21c8ebb669
i32esrah <i32esrah@uco.es>
2025-10-02 17:32:52 +0200

    Server guarreado

--- cliente.c
+++ cliente.c
@@ -1,78 +1,141 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <sys/time.h>
 
 
-int main(int argc, char *argv[]) {
-    if (argc != 5) {
-        fprintf(stderr, "Uso: %s <IP> <PUERTO> <TIMEOUT> <COMANDO>\n", argv[0]);
-        fprintf(stderr, "Ejemplo: %s 127.0.0.1 2000 5 DAYTIME\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+int main ( )
+{
+  
+	/*---------------------------------------------------- 
+		Descriptor del socket y buffer de datos                
+	-----------------------------------------------------*/
+	int sd;
+	struct sockaddr_in sockname;
+	char buffer[250];
+	socklen_t len_sockname;
+    	fd_set readfds, auxfds;
+    	int salida;
+    	int fin = 0;
+	
+    
+	/* --------------------------------------------------
+		Se abre el socket 
+	---------------------------------------------------*/
+  	sd = socket (AF_INET, SOCK_STREAM, 0);
+	if (sd == -1)
+	{
+		perror("No se puede abrir el socket cliente\n");
+    		exit (1);	
+	}
 
+   
+    
+	/* ------------------------------------------------------------------
+		Se rellenan los campos de la estructura con la IP del 
+		servidor y el puerto del servicio que solicitamos
+	-------------------------------------------------------------------*/
+	sockname.sin_family = AF_INET;
+	sockname.sin_port = htons(2000);
+	sockname.sin_addr.s_addr =  inet_addr("127.0.0.1");
 
-    int Socket_Cliente;
-    struct sockaddr_in Servidor;
-    char buffer[100];
-    socklen_t Longitud = sizeof(Servidor);
+	/* ------------------------------------------------------------------
+		Se solicita la conexión con el servidor
+	-------------------------------------------------------------------*/
+	len_sockname = sizeof(sockname);
+	
+	if (connect(sd, (struct sockaddr *)&sockname, len_sockname) == -1)
+	{
+		perror ("Error de conexión");
+		exit(1);
+	}
+    
+    //Inicializamos las estructuras
+    FD_ZERO(&auxfds);
+    FD_ZERO(&readfds);
+    
+    FD_SET(0,&readfds);
+    FD_SET(sd,&readfds);
 
-    // Crear socket UDP
-    if ((Socket_Cliente = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-
-    Servidor.sin_family = AF_INET;
-    Servidor.sin_port = htons(atoi(argv[2]));               // Puerto pasado como argumento
-    Servidor.sin_addr.s_addr = inet_addr(argv[1]);          // IP pasada como argumento
-
-    // Configurar timeout de recepción
-    int timeout = atoi(argv[3]);
-    struct timeval tv;
-    tv.tv_sec = timeout;
-    tv.tv_usec = 0;
-    if (setsockopt(Socket_Cliente, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
-        perror("setsockopt");
-        close(Socket_Cliente);
-        exit(EXIT_FAILURE);
-    }
-
-    char *comando = argv[4];
-    int retries = 0;
-    int success = 0;
-
-    while (retries < 3 && !success) {
-
-        printf("Enviando petición: %s (intento %d)\n", comando, retries + 1);
-        int enviado = sendto(Socket_Cliente, comando, strlen(comando), 0, (struct sockaddr *)&Servidor, Longitud);
-        if (enviado < 0) {
-            printf("Error al enviar la petición\n");
-            close(Socket_Cliente);
-            exit(-1);
+    
+	/* ------------------------------------------------------------------
+		Se transmite la información
+	-------------------------------------------------------------------*/
+	do
+	{
+        auxfds = readfds;
+        salida = select(sd+1,&auxfds,NULL,NULL,NULL);
+        
+        //Tengo mensaje desde el servidor
+        if(FD_ISSET(sd, &auxfds)){
+            
+            bzero(buffer,sizeof(buffer));
+            recv(sd,buffer,sizeof(buffer),0);
+            
+            printf("\n%s\n",buffer);
+            
+            if(strcmp(buffer,"Demasiados clientes conectados\n") == 0)
+                fin =1;
+            
+            if(strcmp(buffer,"Desconexión servidor\n") == 0)
+                fin =1;
+            
+        }
+        else
+        {
+            
+            //He introducido información por teclado
+            if(FD_ISSET(0,&auxfds)){
+                bzero(buffer,sizeof(buffer));
+                
+                fgets(buffer,sizeof(buffer),stdin);
+                
+                if(strcmp(buffer,"SALIR\n") == 0){
+                        fin = 1;
+                
+                }
+                
+                send(sd,buffer,sizeof(buffer),0);
+                
+            }
+            
+            
         }
         
-        int n = recvfrom(Socket_Cliente, buffer, 100, 0, NULL, NULL);
-        if (n >= 0) {
-            buffer[n] = '\0';
-            printf("Respuesta del servidor: %s\n", buffer);
-        } else {
-            retries++;
-            printf("Timeout (%d/%d). Reintentando...\n", retries, 3);
+        
+				
+    }while(fin == 0);
+		
+    close(sd);
 
-            if(retries == 3){
-                success = 1;
-            }
-        }
-    }
-
-    if (!success) {
-        printf("Error: No se recibió respuesta después de %d intentos.\n", 3);
-    }
-
-    close(Socket_Cliente);
     return 0;
+		
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- servidor.c
+++ servidor.c
@@ -1,73 +1,273 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include<signal.h>
+#include <unistd.h>
 #include <time.h>
 #include <arpa/inet.h>
 
-int main ( ){
 
-    int Socket_Servidor;
-    struct sockaddr_in Servidor, Cliente;
-    char Datos[100];
-    socklen_t Longitud;
+#define MSG_SIZE 250
+#define MAX_CLIENTS 20
+#define PORT 2000
 
-    if ((Socket_Servidor = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
-        printf("No se puede abrir el socket\n");
-        exit(-1);
-    }
 
-    Servidor.sin_family = AF_INET;
-	Servidor.sin_port = htons(2000);
-	Servidor.sin_addr.s_addr = htonl(INADDR_ANY);
+/*
+ * El servidor ofrece el servicio de un chat
+ */
 
-    if (bind(Socket_Servidor, (struct sockaddr *)&Servidor, sizeof(Servidor)) == -1){
-        printf("Error de asociacion del puerto\n");
-        close(Socket_Servidor);
-        exit(-1);
-    }
+void manejador(int signum);
+void salirCliente(int socket, fd_set * readfds, int * numClientes, int arrayClientes[]);
 
-    printf("Servidor UDP escuchando en puerto %d...\n", 2000);
 
-    while(1){
-        Longitud = sizeof(Cliente);
-        int recibido = recvfrom(Socket_Servidor, Datos, sizeof(Datos), 0, (struct sockaddr *)&Cliente, &Longitud);
-        if (recibido < 0 ){
-            printf("Error al recibir\n");
-            close(Socket_Servidor);
-            exit(-1);
-        }
-        Datos[recibido] = '\0';
 
-        //Obtener fecha actual
-        time_t t = time(NULL);
-        struct tm *tm_info = localtime(&t);
-        char respuesta[100];
-
-        if(strcmp(Datos, "DAY") == 0){
-            strftime(respuesta, sizeof(respuesta), "%A, %d de %B de %Y", tm_info);
-        }
-        else if(strcmp(Datos, "TIME") == 0){
-            strftime(respuesta, sizeof(respuesta), "%H:%M:%S", tm_info);
-        }
-        else if(strcmp(Datos, "DAYTIME") == 0){
-            strftime(respuesta, sizeof(respuesta), "%A, %d de %B de %Y; %H:%M:%S", tm_info);
-        }
-        else{
-            snprintf(respuesta, sizeof(respuesta), "Error, comando desconocido");
-        }
-
-        sendto(Socket_Servidor, respuesta, strlen(respuesta), 0, (struct sockaddr *)&Cliente, Longitud);
-        printf("Peticion: '%s' enviada\n", Datos);
-    }
-
-    close(Socket_Servidor);
-
-    return 0;
-        
+int main ( )
+{
+  
+	/*---------------------------------------------------- 
+		Descriptor del socket y buffer de datos                
+	-----------------------------------------------------*/
+	int sd, new_sd;
+	struct sockaddr_in sockname, from;
+	char buffer[MSG_SIZE];
+	socklen_t from_len;
+    	fd_set readfds, auxfds;
+   	 int salida;
+   	 int arrayClientes[MAX_CLIENTS];
+    	int numClientes = 0;
+   	 //contadores
+    	int i,j,k;
+	int recibidos;
+   	 char identificador[MSG_SIZE];
     
+    	int on, ret;
+
+    
+    
+	/* --------------------------------------------------
+		Se abre el socket 
+	---------------------------------------------------*/
+  	sd = socket (AF_INET, SOCK_STREAM, 0);
+	if (sd == -1)
+	{
+		perror("No se puede abrir el socket cliente\n");
+    	exit (1);	
+	}
+    
+    	// Activaremos una propiedad del socket para permitir· que otros
+    	// sockets puedan reutilizar cualquier puerto al que nos enlacemos.
+    	// Esto permite· en protocolos como el TCP, poder ejecutar un
+    	// mismo programa varias veces seguidas y enlazarlo siempre al
+   	 // mismo puerto. De lo contrario habrÌa que esperar a que el puerto
+    	// quedase disponible (TIME_WAIT en el caso de TCP)
+    on=1;
+    ret = setsockopt( sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+
+
+	sockname.sin_family = AF_INET;
+	sockname.sin_port = htons(PORT);
+	sockname.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind (sd, (struct sockaddr *) &sockname, sizeof (sockname)) == -1)
+	{
+		perror("Error en la operación bind");
+		exit(1);
+	}
+	
+
+   	/*---------------------------------------------------------------------
+		Del las peticiones que vamos a aceptar sólo necesitamos el 
+		tamaño de su estructura, el resto de información (familia, puerto, 
+		ip), nos la proporcionará el método que recibe las peticiones.
+   	----------------------------------------------------------------------*/
+	from_len = sizeof (from);
+
+
+	if(listen(sd,1) == -1){
+		perror("Error en la operación de listen");
+		exit(1);
+	}
+
+	
+    printf("El servidor está esperando conexiones...\n");	//Inicializar los conjuntos fd_set
+    	
+	FD_ZERO(&readfds);
+    FD_ZERO(&auxfds);
+    FD_SET(sd,&readfds);
+    FD_SET(0,&readfds);
+    
+   	
+    //Capturamos la señal SIGINT (Ctrl+c)
+    signal(SIGINT,manejador);
+    
+	/*-----------------------------------------------------------------------
+		El servidor acepta una petición
+	------------------------------------------------------------------------ */
+	while(1){
+            
+            //Esperamos recibir mensajes de los clientes (nuevas conexiones o mensajes de los clientes ya conectados)
+            
+            auxfds = readfds;
+            
+            salida = select(FD_SETSIZE,&auxfds,NULL,NULL,NULL);
+            
+            if(salida > 0){
+                
+                
+                for(i=0; i<FD_SETSIZE; i++){
+                    
+                    //Buscamos el socket por el que se ha establecido la comunicación
+                    if(FD_ISSET(i, &auxfds)) {
+                        
+                        if( i == sd){
+                            
+                            if((new_sd = accept(sd, (struct sockaddr *)&from, &from_len)) == -1){
+                                perror("Error aceptando peticiones");
+                            }
+                            else
+                            {
+                                if(numClientes < MAX_CLIENTS){
+                                    arrayClientes[numClientes] = new_sd;
+                                    numClientes++;
+                                    FD_SET(new_sd,&readfds);
+                                
+                                    strcpy(buffer, "+Ok. Usuario conectado.\n");
+                                
+                                    send(new_sd,buffer,sizeof(buffer),0);
+
+                                    recibidos = recv(i, buffer, sizeof(buffer), 0);
+
+                                    if(recibidos > 0){
+                                        //Hacer funcion que convierta el mensaje en un vector de cadenas.
+                                    }
+                                
+                                    for(j=0; j<(numClientes-1);j++){
+                                    
+                                        bzero(buffer,sizeof(buffer));
+                                        sprintf(buffer, "Nuevo Cliente conectado en <%d>",new_sd);
+                                        send(arrayClientes[j],buffer,sizeof(buffer),0);
+                                    }
+                                }
+                                else
+                                {
+                                    bzero(buffer,sizeof(buffer));
+                                    strcpy(buffer,"Demasiados clientes conectados\n");
+                                    send(new_sd,buffer,sizeof(buffer),0);
+                                    close(new_sd);
+                                }
+                                
+                            }
+                            
+                            
+                        }
+                        else if (i == 0){
+                            //Se ha introducido información de teclado
+                            bzero(buffer, sizeof(buffer));
+                            fgets(buffer, sizeof(buffer),stdin);
+                            
+                            //Controlar si se ha introducido "SALIR", cerrando todos los sockets y finalmente saliendo del servidor. (implementar)
+                            if(strcmp(buffer,"SALIR\n") == 0){
+                             
+                                for (j = 0; j < numClientes; j++){
+						            bzero(buffer, sizeof(buffer));
+						            strcpy(buffer,"Desconexión servidor\n"); 
+                                    send(arrayClientes[j],buffer , sizeof(buffer),0);
+                                    close(arrayClientes[j]);
+                                    FD_CLR(arrayClientes[j],&readfds);
+                                }
+                                    close(sd);
+                                    exit(-1);
+                                
+                                
+                            }
+                            //Mensajes que se quieran mandar a los clientes (implementar)
+                            
+                        } 
+                        else{
+                            bzero(buffer,sizeof(buffer));
+                            
+                            recibidos = recv(i,buffer,sizeof(buffer),0);
+                            
+                            if(recibidos > 0){
+                                
+                                if(strcmp(buffer,"SALIR\n") == 0){
+                                    
+                                    salirCliente(i,&readfds,&numClientes,arrayClientes);
+                                    
+                                }
+                                else{
+                                    
+                                    sprintf(identificador,"<%d>: %s",i,buffer);
+                                    bzero(buffer,sizeof(buffer));
+
+                                    strcpy(buffer,identificador);
+
+                                    printf("%s\n", buffer);
+
+                                    for(j=0; j<numClientes; j++)
+                                        if(arrayClientes[j] != i)
+                                            send(arrayClientes[j],buffer,sizeof(buffer),0);
+
+                                    
+                                }
+                                                                
+                                
+                            }
+                            //Si el cliente introdujo ctrl+c
+                            if(recibidos== 0)
+                            {
+                                printf("El socket %d, ha introducido ctrl+c\n", i);
+                                //Eliminar ese socket
+                                salirCliente(i,&readfds,&numClientes,arrayClientes);
+                            }
+                        }
+                    }
+                }
+            }
+		}
+
+	close(sd);
+	return 0;
+	
+}
+
+void salirCliente(int socket, fd_set * readfds, int * numClientes, int arrayClientes[]){
+  
+    char buffer[250];
+    int j;
+    
+    close(socket);
+    FD_CLR(socket,readfds);
+    
+    //Re-estructurar el array de clientes
+    for (j = 0; j < (*numClientes) - 1; j++)
+        if (arrayClientes[j] == socket)
+            break;
+    for (; j < (*numClientes) - 1; j++)
+        (arrayClientes[j] = arrayClientes[j+1]);
+    
+    (*numClientes)--;
+    
+    bzero(buffer,sizeof(buffer));
+    sprintf(buffer,"Desconexión del cliente <%d>",socket);
+    
+    for(j=0; j<(*numClientes); j++)
+        if(arrayClientes[j] != socket)
+            send(arrayClientes[j],buffer,sizeof(buffer),0);
+
+
+}
+
+
+void manejador (int signum){
+    printf("\nSe ha recibido la señal de salida\n");
+    signal(SIGINT,manejador);
+    
+    //Implementar lo que se desee realizar cuando ocurra la excepción de ctrl+c en el servidor
 }
